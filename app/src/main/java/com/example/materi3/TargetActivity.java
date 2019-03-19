6f45c14fa115074d49b2e5cad82c37f97a511dcb
Dhona <alfina_dhona_26rpl@student.smktelkom-mlg.sch.id>
2019-03-19 10:48:46 +0700

    first commit

---
+++ app/src/main/java/com/example/materi3/TargetActivity.java
@@ -0,0 +1,19 @@
+package com.example.materi3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TargetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target);
+        TextView txtResult = (TextView) findViewById(R.id.textView);
+        //get data         
+        if(getIntent().getExtras() != null ){
+            String data = getIntent().getExtras().getString(MainActivity. EXTRA_DATA );
+            txtResult.setText(data);
+    }
+} }
